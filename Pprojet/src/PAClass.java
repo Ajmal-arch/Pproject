8bc338a577051083aa48f5e80122f2e8d902e62c
T440s <T440s@DESKTOP-U29E12C>
2019-09-28 11:33:24 -0400

    Automated9900 new line of code added

---
+++ Pprojet/src/PAClass.java
@@ -0,0 +1,9 @@
+
+public class PAClass {
+	public static void main(String[] args) {
+		System.out.println("iam working in my project");
+        System.out.println("i am new in programing");
+	}
+	
+
+}
